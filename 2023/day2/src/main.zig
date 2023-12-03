e81b94f911c2d1a66f9919feb120a53320778959
Jake Helfrich <jakenichelfrich@gmail.com>
2023-12-03 18:25:42 +0800

    Abstraction for parsing game info

--- 2023/day2/src/main.zig
+++ 2023/day2/src/main.zig
@@ -5,33 +5,67 @@ pub fn main() !void {
     defer file.close();
 
     const reader = file.reader();
-    var varBuffer = std.ArrayList(u8).init(std.heap.page_allocator);
 
     var gameNum: i32 = 1;
-    while (reader.streamUntilDelimiter(varBuffer.writer(), '\n', null)) {
-        const line = varBuffer.allocatedSlice();
-        var lineItr = std.mem.splitSequence(u8, line, ":");
-        _ = lineItr.next().?; // We don't use the game information
-
-        const setsLine: []const u8 = lineItr.next().?;
-        var setsItr = std.mem.tokenizeSequence(u8, setsLine, ";");
-
-        while (setsItr.next()) |set| {
-            var cubeIter = std.mem.tokenizeSequence(u8, set, ",");
-            while (cubeIter.next()) |_cube| {
-                var cubeInfo = std.mem.trim(u8, _cube, " ");
-                var cubeInfoItr = std.mem.splitSequence(u8, cubeInfo, " ");
-                var number = cubeInfoItr.next().?;
-                var colour = cubeInfoItr.next().?;
-                std.debug.print("|{s} {s}|", .{ colour, number });
-            }
-        }
-        std.debug.print("\n", .{});
-
+    while (readNextGame(reader)) |gameInfo| {
+        const firstSet = gameInfo.items[0];
+        std.debug.print("Game {d}: Total Red = {d} \n", .{ gameNum, firstSet.red });
         gameNum += 1;
-        varBuffer.clearAndFree();
-        break;
     } else |err| {
         _ = err catch null;
     }
+}
+
+const Colour = enum(u8) {
+    red,
+    green,
+    blue,
+};
+
+const SetInfo = struct {
+    red: i32,
+    green: i32,
+    blue: i32,
+};
+
+const GameInfo = std.ArrayList(SetInfo);
+
+fn readNextGame(reader: std.fs.File.Reader) !GameInfo {
+    var varBuffer = std.ArrayList(u8).init(std.heap.page_allocator);
+    defer varBuffer.clearAndFree();
+
+    try reader.streamUntilDelimiter(varBuffer.writer(), '\n', null);
+
+    const line = varBuffer.allocatedSlice();
+    var lineItr = std.mem.splitSequence(u8, line, ":");
+    _ = lineItr.next().?; // We don't use the game information
+
+    const setsInfo: []const u8 = lineItr.next().?;
+    var setsItr = std.mem.tokenizeSequence(u8, setsInfo, ";");
+
+    var gameInfo = GameInfo.init(std.heap.page_allocator);
+    while (setsItr.next()) |set| {
+        var cubeIter = std.mem.tokenizeSequence(u8, set, ",");
+        var setInfo = SetInfo{
+            .red = 0,
+            .green = 0,
+            .blue = 0,
+        };
+        while (cubeIter.next()) |cube| {
+            var cubeInfo = std.mem.trim(u8, cube, " \x00");
+            var cubeInfoItr = std.mem.splitSequence(u8, cubeInfo, " ");
+            var number = std.fmt.parseInt(i32, cubeInfoItr.next().?, 10) catch 0;
+            var pre = cubeInfoItr.next().?;
+            var colour: Colour = std.meta.stringToEnum(Colour, pre).?;
+
+            switch (colour) {
+                Colour.red => setInfo.red += number,
+                Colour.blue => setInfo.blue += number,
+                Colour.green => setInfo.green += number,
+            }
+        }
+        try gameInfo.append(setInfo);
+    }
+
+    return gameInfo;
 }
