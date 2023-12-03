964774a05808471581c05a4b2470509279eb775c
Jake Helfrich <jakenichelfrich@gmail.com>
2023-12-03 20:15:18 +0800

    (day 3) Add parseBoard abstraction

--- 2023/day3/src/main.zig
+++ 2023/day3/src/main.zig
@@ -4,8 +4,33 @@ pub fn main() !void {
     const file = try std.fs.cwd().openFile("example.txt", .{ .mode = .read_only });
     var reader = file.reader();
 
+    var board = try parseBoard(reader);
+    _ = board;
+}
+
+const Number = struct { value: i32, y: i32, x: struct { i32, i32 } };
+
+const Pattern = struct {
+    x: i32,
+    y: i32,
+};
+
+const Board = struct {
+    numbers: []Number,
+    patterns: []Pattern,
+};
+
+fn parseBoard(reader: anytype) !Board {
+    var numbers = std.ArrayList(Number).init(std.heap.page_allocator);
+    var patterns = std.ArrayList(Pattern).init(std.heap.page_allocator);
+
     var varBuffer = std.ArrayList(u8).init(std.heap.page_allocator);
     try reader.streamUntilDelimiter(varBuffer.writer(), '\n', null);
 
     std.debug.print("{s} \n", .{varBuffer.items});
+
+    return Board{
+        .numbers = numbers.items,
+        .patterns = patterns.items,
+    };
 }
