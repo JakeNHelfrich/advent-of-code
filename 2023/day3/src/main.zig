6087336e017ebf650df73a4c180e7d1cf3c08f45
Jake Helfrich <jakenichelfrich@gmail.com>
2023-12-03 20:46:37 +0800

    (day 3) implement parse board game

--- 2023/day3/src/main.zig
+++ 2023/day3/src/main.zig
@@ -2,10 +2,18 @@ const std = @import("std");
 
 pub fn main() !void {
     const file = try std.fs.cwd().openFile("example.txt", .{ .mode = .read_only });
+    defer file.close();
     var reader = file.reader();
 
     var board = try parseBoard(reader);
-    _ = board;
+
+    for (board.numbers) |number| {
+        std.debug.print("Value {d} | y {d} | x start {d} x end {d} \n", .{ number.value, number.y, number.x[0], number.x[1] });
+    }
+
+    for (board.patterns) |pattern| {
+        std.debug.print("Pattern | y {d} | x {d} \n", .{ pattern.y, pattern.x });
+    }
 }
 
 const Number = struct { value: i32, y: i32, x: struct { i32, i32 } };
@@ -25,9 +33,36 @@ fn parseBoard(reader: anytype) !Board {
     var patterns = std.ArrayList(Pattern).init(std.heap.page_allocator);
 
     var varBuffer = std.ArrayList(u8).init(std.heap.page_allocator);
-    try reader.streamUntilDelimiter(varBuffer.writer(), '\n', null);
+    defer varBuffer.clearAndFree();
 
-    std.debug.print("{s} \n", .{varBuffer.items});
+    var row: i32 = 0;
+    while (reader.streamUntilDelimiter(varBuffer.writer(), '\n', null)) : (varBuffer.clearAndFree()) {
+        const line: []const u8 = std.mem.trim(u8, varBuffer.allocatedSlice(), "\x00");
+        var curr: usize = 0;
+        while (curr < line.len) : (curr += 1) {
+            var char = line[curr];
+
+            if (std.ascii.isDigit(char)) {
+                var numberList = std.ArrayList(u8).init(std.heap.page_allocator);
+                var start: i32 = @intCast(curr);
+                while (std.ascii.isDigit(line[curr])) : (curr += 1) {
+                    char = line[curr];
+                    try numberList.append(char);
+                }
+                curr -= 1;
+                var end: i32 = @intCast(curr);
+
+                const number = Number{ .value = try std.fmt.parseInt(i32, numberList.items, 10), .y = row, .x = .{ start, end } };
+                try numbers.append(number);
+            } else if (!std.ascii.isAlphanumeric(char) and char != '.') {
+                const pattern = Pattern{ .y = row, .x = @intCast(curr) };
+                try patterns.append(pattern);
+            }
+        }
+        row += 1;
+    } else |err| {
+        _ = err catch null;
+    }
 
     return Board{
         .numbers = numbers.items,
