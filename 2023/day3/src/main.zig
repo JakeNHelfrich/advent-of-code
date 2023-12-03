5fab94106ab09881980ab19a1aacd54dd9dd82ba
Jake Helfrich <jakenichelfrich@gmail.com>
2023-12-03 21:06:38 +0800

    (day 3.1) solved

--- 2023/day3/src/main.zig
+++ 2023/day3/src/main.zig
@@ -1,19 +1,34 @@
 const std = @import("std");
 
 pub fn main() !void {
-    const file = try std.fs.cwd().openFile("example.txt", .{ .mode = .read_only });
+    const file = try std.fs.cwd().openFile("inputs.txt", .{ .mode = .read_only });
     defer file.close();
     var reader = file.reader();
 
     var board = try parseBoard(reader);
 
+    var sum: i32 = 0;
     for (board.numbers) |number| {
-        std.debug.print("Value {d} | y {d} | x start {d} x end {d} \n", .{ number.value, number.y, number.x[0], number.x[1] });
+        var nextToPattern = false;
+        for (board.patterns) |pattern| {
+            var directlyAdjacent = std.math.absCast(number.y - pattern.y) == 1;
+            directlyAdjacent = directlyAdjacent and pattern.x >= number.x[0] and pattern.x <= number.x[1];
+            directlyAdjacent = directlyAdjacent or (std.math.absCast(number.x[0] - pattern.x) == 1 and number.y == pattern.y);
+            directlyAdjacent = directlyAdjacent or (std.math.absCast(number.x[1] - pattern.x) == 1 and number.y == pattern.y);
+
+            var diagonallyAdjacent = std.math.absCast(number.y - pattern.y) == 1;
+            diagonallyAdjacent = diagonallyAdjacent and
+                (std.math.absCast(number.x[0] - pattern.x) == 1 or std.math.absCast(number.x[1] - pattern.x) == 1);
+
+            nextToPattern = nextToPattern or (directlyAdjacent or diagonallyAdjacent);
+        }
+
+        if (nextToPattern) {
+            sum += number.value;
+        }
     }
 
-    for (board.patterns) |pattern| {
-        std.debug.print("Pattern | y {d} | x {d} \n", .{ pattern.y, pattern.x });
-    }
+    std.debug.print("Sum: {d}\n", .{sum});
 }
 
 const Number = struct { value: i32, y: i32, x: struct { i32, i32 } };
@@ -45,7 +60,7 @@ fn parseBoard(reader: anytype) !Board {
             if (std.ascii.isDigit(char)) {
                 var numberList = std.ArrayList(u8).init(std.heap.page_allocator);
                 var start: i32 = @intCast(curr);
-                while (std.ascii.isDigit(line[curr])) : (curr += 1) {
+                while (curr < line.len and std.ascii.isDigit(line[curr])) : (curr += 1) {
                     char = line[curr];
                     try numberList.append(char);
                 }
