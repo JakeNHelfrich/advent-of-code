dcbb50d8fe406db5c5f367f1c8c75f1df8e0901d
Jake Helfrich <jakenichelfrich@gmail.com>
2023-12-11 21:28:50 +0800

    (day 6) read file

--- 2023/day6/src/main.zig
+++ 2023/day6/src/main.zig
@@ -1,24 +1,15 @@
 const std = @import("std");
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    const file = try std.fs.cwd().openFile("example.txt", .{ .mode = .read_only });
+    defer file.close();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    var reader = file.reader();
+    var buffer = std.ArrayList(u8).init(std.heap.page_allocator);
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+    while (reader.streamUntilDelimiter(buffer.writer(), '\n', null)) : (buffer.clearAndFree()) {
+        std.debug.print("{s} \n", .{buffer.items});
+    } else |err| {
+        _ = err catch null;
+    }
 }
