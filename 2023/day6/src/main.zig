b59b9098632ed42101f48689d87349c74491701a
Jake Helfrich <jakenichelfrich@gmail.com>
2023-12-11 21:46:18 +0800

    (day 6) parse races

--- 2023/day6/src/main.zig
+++ 2023/day6/src/main.zig
@@ -5,11 +5,40 @@ pub fn main() !void {
     defer file.close();
 
     var reader = file.reader();
-    var buffer = std.ArrayList(u8).init(std.heap.page_allocator);
+    var races = try parseRaces(reader, std.heap.page_allocator);
 
-    while (reader.streamUntilDelimiter(buffer.writer(), '\n', null)) : (buffer.clearAndFree()) {
-        std.debug.print("{s} \n", .{buffer.items});
-    } else |err| {
-        _ = err catch null;
+    std.debug.print("{d} {d} \n", .{ races[0].time, races[0].distance });
+}
+
+const Race = struct {
+    time: i64,
+    distance: i64,
+};
+
+fn parseRaces(reader: anytype, alloc: std.mem.Allocator) ![]Race {
+    var races = std.ArrayList(Race).init(alloc);
+
+    var timeBuffer = std.ArrayList(u8).init(alloc);
+    defer timeBuffer.clearAndFree();
+    try reader.streamUntilDelimiter(timeBuffer.writer(), '\n', null);
+
+    var timeIter = std.mem.tokenizeSequence(u8, timeBuffer.items, " ");
+    _ = timeIter.next();
+
+    var distanceBuffer = std.ArrayList(u8).init(alloc);
+    defer distanceBuffer.clearAndFree();
+    try reader.streamUntilDelimiter(distanceBuffer.writer(), '\n', null);
+
+    var distanceIter = std.mem.tokenizeSequence(u8, distanceBuffer.items, " ");
+    _ = distanceIter.next();
+
+    while (timeIter.next()) |timeStr| {
+        var distanceStr = distanceIter.next().?;
+        var time = try std.fmt.parseInt(i64, std.mem.trim(u8, timeStr, "\x00"), 10);
+        var distance = try std.fmt.parseInt(i64, std.mem.trim(u8, distanceStr, "\x00"), 10);
+        const race = Race{ .time = time, .distance = distance };
+        try races.append(race);
     }
+
+    return races.items;
 }
