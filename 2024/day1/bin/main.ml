a8725246c1424d4bce65210d5fe12fa9dd24b89b
Jake Helfrich <jakenichelfrich@gmail.com>
2024-12-04 21:16:48 +0800

    day 1: read first line

--- 2024/day1/bin/main.ml
+++ 2024/day1/bin/main.ml
@@ -1,2 +1,7 @@
-let data = open_in "data/sample.txt";
-(* TODO: Print first line of file *)
+let () =
+  let input_chan = open_in "data/sample.txt" in 
+  try
+    let line = input_line input_chan in
+    print_endline line;
+  with _ ->
+    close_in_noerr input_chan;
