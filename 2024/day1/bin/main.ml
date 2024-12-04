83d0293c3c775e88e687ad98d4c01b6d8ea1acd2
Jake Helfrich <jakenichelfrich@gmail.com>
2024-12-04 21:19:43 +0800

    day 1: read and print file

--- 2024/day1/bin/main.ml
+++ 2024/day1/bin/main.ml
@@ -1,7 +1,9 @@
 let () =
   let input_chan = open_in "data/sample.txt" in 
   try
-    let line = input_line input_chan in
-    print_endline line;
+    while true do
+      let line = input_line input_chan in
+      print_endline line;
+    done
   with _ ->
     close_in_noerr input_chan;
