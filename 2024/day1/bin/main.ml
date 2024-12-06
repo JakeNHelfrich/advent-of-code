6877efc14c559667a41441ec9a4c1b317a403935
Jake Helfrich <jakenichelfrich@gmail.com>
2024-12-06 21:47:24 +0800

    day 1: lift read&print into function

--- 2024/day1/bin/main.ml
+++ 2024/day1/bin/main.ml
@@ -1,9 +1,12 @@
-let () =
-  let input_chan = open_in "data/sample.txt" in 
+let print_file file_name = begin
+  let ic = open_in file_name in
   try
     while true do
-      let line = input_line input_chan in
-      print_endline line;
+      let line = input_line ic in
+      print_endline line      
     done
   with _ ->
-    close_in_noerr input_chan;
+    close_in_noerr ic
+end
+
+let _ = print_file "data/sample.txt"
